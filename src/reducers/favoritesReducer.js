1673201f712e10d72fc8c19550e2fdb31a550a08
Hailey Magoline <magoline.hailey@gmail.com>
2022-01-19 01:57:10 -0600

    finished filling out favorites reducer and favorites action

--- src/reducers/favoritesReducer.js
+++ src/reducers/favoritesReducer.js
@@ -1,3 +1,5 @@
+import { ADD_FAVORITES, DELETE_FAVORITES, TOGGLE_FAVORITES } from '../actions/favoritesActions'
+
 const initialState = {
     favorites: [],
     displayFavorites: true
@@ -5,6 +7,24 @@ const initialState = {
 
 const favoritesReducer = (state = initialState, action) => {
     switch(action.type) {
+
+        case ADD_FAVORITES:
+
+            return {
+                ...state, 
+                favorites: [...state.favorites, action.payload]
+            }
+        case DELETE_FAVORITES:
+            return {
+                ...state, 
+                favorites: state.favorites.filter(item=>(action.payload !== item.id))
+            }
+        case TOGGLE_FAVORITES:
+            return {
+                ...state, 
+                displayFavorites: !state.displayFavorites
+            }
+
         default:
             return (state);
     }
